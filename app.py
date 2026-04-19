33166c240b94ccb0f4a5b5e21699d88c90b49505
Dimitri Merejkowsky <dimitri@dmerej.info>
2026-04-19 13:12:28 +0200

    Premier commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, send_from_directory
+
+app = Flask("petitions")
+
+
+@app.get("/")
+def index():
+    return render_template("index.html", nombre_signatures=10)
+
+
+@app.get("/style.css")
+def sytle():
+    return send_from_directory("static", "style.css")
