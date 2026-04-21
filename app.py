e3c50fa2c58909ae3953eab7d94f7c9655c1be7c
Quentin Borde <quentin.borde@etu.minesparis.psl.eu>
2026-04-21 15:25:01 +0200

    correction 1

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 
 app = Flask("petitions")
+
+NOMBRE_DE_SIGNATURES = 0
 
 
 @app.get("/")
 def index():
-    return render_template("index.html", nombre_signatures=1)
+    return render_template("index.html", nombre_signatures=NOMBRE_DE_SIGNATURES)
+
+
+@app.post("/")
+def sign():
+    global NOMBRE_DE_SIGNATURES
+    NOMBRE_DE_SIGNATURES += 1
+    print(request.form)
 
 
 @app.get("/style.css")
